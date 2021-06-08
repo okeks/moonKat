e3cfa36fef59e85e6e8b494e8c847297c2466e53
usman <uzlash16@gmail.com>
2021-06-08 14:43:56 +0100

    added countdown

--- js/customJs.js
+++ js/customJs.js
@@ -1,8 +1,9 @@
 // document.addEventListener('DOMContentLoaded', () => {
 
 // Unix timestamp (in seconds) to count down to
-var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
 
+// var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
+var twoDaysFromNow = (new Date('2021.06.10').getTime() / 1000) + (64800)
 // Set up FlipDown
 var flipdown = new FlipDown(twoDaysFromNow, {
     theme: 'light' // or dark
